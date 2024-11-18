7b7dec954687c0f9c53045d3c8b51911fe3950e3
sanBastia <irsanoncode@gmail.com>
2024-11-18 14:18:26 +0700

    feat: add category badges component below headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
-import SearchInput from "@/components/searchInput";
+
+import CategoryBadges from "@/components/CategoryBadges";
+import SearchInput from "@/components/SearchInput";
+
+
 
 export default function Home() {
   return (
-    <>
-    <header className="w-full">
-        <nav className="max-w-[1440px] mx-auto flex justify-between items-center px-10 2xl:px-36 py-6">
-              <div className='flex item-centers'>
-                <h1 className="font-bold font-sans text-2xl">
-                Harga Crypto dalam Rupiah hari ini
-                </h1>
-            </div>
-            <div className='flex-end items-center'>
-            <SearchInput />
-        </div>
-            </nav>
-      </header>
-   
- </>
+          <div>
+              <header className="w-full">
+                  <nav className="flex justify-between items-center px-10 2xl:px-36 py-6">
+                        <div className='flex item-centers'>
+                          <h1 className="font-bold font-sans text-2xl">
+                          Harga Crypto dalam Rupiah hari ini
+                          </h1>
+                      </div>
+                      <div className='flex-end items-center'>
+                      <SearchInput />
+                  </div>
+                      </nav>
+                </header>
+
+                  <div className="w-full">
+                
+                      <div className="flex justify-around px-10 2xl:px-36">
+                        <CategoryBadges />
+                       </div>
+                  </div>
+              </div>
   );
 }
