dd6cafdaa3a22fa6065f662e10901b1b3616a323
sanBastia <irsanoncode@gmail.com>
2024-11-18 12:37:11 +0700

    style: layout for header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import SearchInput from "@/components/searchInput";
+
 export default function Home() {
   return (
- <div>Harga Crypto dalam rupiah hari ini</div>
+    <>
+    <header className="w-full">
+        <nav className="max-w-[1440px] mx-auto flex justify-between items-center px-10 2xl:px-36 py-6">
+              <div className='flex item-centers'>
+                <h1 className="font-bold font-sans text-2xl">
+                Harga Crypto dalam Rupiah hari ini
+                </h1>
+            </div>
+            <div className='flex-end items-center'>
+            <SearchInput />
+        </div>
+            </nav>
+      </header>
+   
+ </>
   );
 }
