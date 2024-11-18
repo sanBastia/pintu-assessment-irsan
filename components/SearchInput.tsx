f9fb498eac2a070a51e7b390498a39d441b8ed46
sanBastia <irsanoncode@gmail.com>
2024-11-18 14:35:32 +0700

    fix: bug on renaming folder searchInput

---
+++ components/SearchInput.tsx
@@ -0,0 +1,12 @@
+
+import { Input } from "@/components/ui/input"
+import { FaSearch } from "react-icons/fa";
+
+export default function SearchInput() {
+  return (
+    <div className="flex w-full max-w-sm items-center space-x-2">
+      <FaSearch size={20} />
+      <Input type="text" placeholder="Cari Aset di Pintu" />
+    </div>
+  )
+}
