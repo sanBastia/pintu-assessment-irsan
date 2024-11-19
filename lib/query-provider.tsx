a620fd3a87818ba4cfe1a41d1fa2278e5b045b23
sanBastia <irsanoncode@gmail.com>
2024-11-19 10:50:13 +0700

    feat: fetch only one time

--- lib/query-provider.tsx
+++ lib/query-provider.tsx
@@ -14,9 +14,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         defaultOptions: {
           queries: {
             // With SSR, we usually want to set some default staleTime
-            // above 0 to avoid refetching immediately on the client
-            staleTime: 4 * 1000,
-            refetchInterval: 4 * 1000,
+            // fetch only one time
+            staleTime: Infinity,
           },
         },
       })
