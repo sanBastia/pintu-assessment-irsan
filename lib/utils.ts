c3c1b4f384b89b6a07e0bbdf2d01df8cb5229b73
sanBastia <irsanoncode@gmail.com>
2024-11-20 15:04:34 +0700

    feat: simple function for currency format and redgreenindicator

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function CurrrencyFormat(latestPrice: string) {
+  return Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(Number(latestPrice))
+}
+
+export function RedGreenIndicator(persentage: string) {
+ return `${persentage.charAt(0) === '-' ? 'text-red-600': "text-emerald-600"}`
+}
