5193b8791d62585e0b5c82a5e5fbb063c213fbe4
sanBastia <irsanoncode@gmail.com>
2024-11-21 15:28:30 +0700

    chore: remove un-used code

--- lib/utils.ts
+++ lib/utils.ts
@@ -31,30 +31,3 @@ export const combineCurrencyAndPriceChangeData = (
 }
 
 // export const RedGreenIndicator = (persentage: string) => clsx(persentage.charAt(0) === '-' ? 'text-red-600' : 'text-emerald-600');
-
-
-// const SupportedCurrencies = dataSupportedCurrencies.payload.map((curr: {
-//   currencyGroup: string; logo: string; currencySymbol: string; color: string; name: string 
-// }) => ({
-//       currencySymbol: curr.currencySymbol,
-//       currencyGroup: curr.currencyGroup,
-//       color: curr.color,
-//       name: curr.name,
-//       logo: curr.logo,
-// })) 
-// const PriceChange = dataPricechange.payload.map((price: any) => (price))  
-
-
-// const combinedArray: any[] = [];
-
-// SupportedCurrencies.forEach((curr: { currencyGroup: string }) => {
-//     const matchingPair = PriceChange.find((price: { pair: string }) => curr.currencyGroup.toLowerCase() === price.pair.split('/')[0]); // Assuming 'pair' has currencyGroup in first part
-
-//     if (matchingPair) {
-//       combinedArray.push({
-//         ...curr,
-//         ...matchingPair
-//       });
-//     }
-// });
-// console.log(combinedArray, "CA");
