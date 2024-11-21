0a93155389fd601aa5d76e67c779df11ebd456b1
sanBastia <irsanoncode@gmail.com>
2024-11-21 21:49:03 +0700

    refactor: refactor table header sort

--- components/Tables.tsx
+++ components/Tables.tsx
@@ -1,79 +1,139 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-'use client'
+"use client";
 
-import { useGetPriceChanges, useGetSupportedCurrencies } from '@/data/get-data'
-import React, { useState } from 'react'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
-import { combineCurrencyAndPriceChangeData, CurrrencyFormat } from '@/lib/utils'
-import SearchInput from './SearchInput'
-import { LoadingSpinner } from './LoadingSpinner'
-import CoinLogo from './CoinLogo'
-import { FaChevronUp, FaChevronDown } from "react-icons/fa";
-import TableCellPersentage from './TableCellPersentage'
+import { useGetPriceChanges, useGetSupportedCurrencies } from "@/data/get-data";
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import {
+  combineCurrencyAndPriceChangeData,
+  CurrrencyFormat,
+} from "@/lib/utils";
+import SearchInput from "./SearchInput";
+import { LoadingSpinner } from "./LoadingSpinner";
+import CoinLogo from "./CoinLogo";
+// import { FaChevronUp, FaChevronDown } from "react-icons/fa";
+import TableCellPersentage from "./TableCellPersentage";
+import TableHeaderSort from "./TableHeaderSort";
 
 export default function Tables() {
-  
-  const { data: dataPricechange, error: errorPriceChange, isLoading: isLoadingPriceChange } = useGetPriceChanges()
-  const { data: dataSupportedCurrencies, error: errorSupportedCurrencies, isLoading: isLoadingSupportedCurrencies } = useGetSupportedCurrencies()
+  const {
+    data: dataPricechange,
+    error: errorPriceChange,
+    isLoading: isLoadingPriceChange,
+  } = useGetPriceChanges();
+  const {
+    data: dataSupportedCurrencies,
+    error: errorSupportedCurrencies,
+    isLoading: isLoadingSupportedCurrencies,
+  } = useGetSupportedCurrencies();
 
-  const [searchTerm, setSearchTerm] = useState('')
-  const [sortBy, setSortBy] = useState('')
-  const [sortOrder,setSortOrder] = useState('high')
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [sortOrder, setSortOrder] = useState("high");
 
- 
-  if (errorPriceChange && errorSupportedCurrencies) return errorPriceChange.message 
+  if (errorPriceChange && errorSupportedCurrencies)
+    return errorPriceChange.message;
 
-  if(isLoadingPriceChange || isLoadingSupportedCurrencies ){
+  if (isLoadingPriceChange || isLoadingSupportedCurrencies) {
     return (
       <div className="w-full">
-        <div className='flex px-10 2xl:px-36 my-4'>
-        <LoadingSpinner />
+        <div className="flex px-10 2xl:px-36 my-4">
+          <LoadingSpinner />
+        </div>
       </div>
-      </div>
-    )
-  } 
+    );
+  }
 
-  if(dataPricechange && dataSupportedCurrencies){
-      
-        const supportedCurrencies = dataSupportedCurrencies.payload
-        const priceChanges = dataPricechange.payload
-        const combined = combineCurrencyAndPriceChangeData(supportedCurrencies, priceChanges);
+  if (dataPricechange && dataSupportedCurrencies) {
+    const supportedCurrencies = dataSupportedCurrencies.payload;
+    const priceChanges = dataPricechange.payload;
+    const combined = combineCurrencyAndPriceChangeData(
+      supportedCurrencies,
+      priceChanges
+    );
 
-          // Function to handle sorting
-          const handleSort = (key: string) => {
-        
-            if (sortBy === key) {
-              setSortOrder(sortOrder === "high" ? "low" : "high");
-            } else {
-              setSortBy(key);
-              setSortOrder("high");
-            }
-          };
-  
-          // Sorting logic based on sortBy and sortOrder
-          const sortedData = [...combined].sort((a, b) => {
-            const aValue = parseInt(a[sortBy]);
-            const bValue = parseInt(b[sortBy]);
+    // Function to handle sorting
+    // const handleSort = (key: string) => {
 
-            if (sortOrder === "high") {
-              return aValue < bValue ? -1 : aValue > bValue ? 1 : 0;
-            } else {
-              return bValue < aValue ? -1 : bValue > aValue ? 1 : 0;
-            }
-          });
-            
-                
+    //   if (sortBy === key) {
+    //     setSortOrder(sortOrder === "high" ? "low" : "high");
+    //   } else {
+    //     setSortBy(key);
+    //     setSortOrder("high");
+    //   }
+    // };
+
+    // Sorting logic based on sortBy and sortOrder
+    const sortedData = [...combined].sort((a, b) => {
+      const aValue = parseInt(a[sortBy]);
+      const bValue = parseInt(b[sortBy]);
+
+      if (sortOrder === "high") {
+        return aValue < bValue ? -1 : aValue > bValue ? 1 : 0;
+      } else {
+        return bValue < aValue ? -1 : bValue > aValue ? 1 : 0;
+      }
+    });
+
     return (
       <div className="w-full">
-      <div className='flex px-10 2xl:px-36 my-4'>
-        <SearchInput searchTerm={searchTerm} handleSearch={setSearchTerm} />
-      </div>
-      <div className="flex px-10 2xl:px-36">
+        <div className="flex px-10 2xl:px-36 my-4">
+          <SearchInput searchTerm={searchTerm} handleSearch={setSearchTerm} />
+          <button>X</button>
+        </div>
+        <div className="flex px-10 2xl:px-36">
           <Table>
             <TableHeader>
               <TableRow>
                 <TableHead colSpan={2}>Crypto</TableHead>
-                <TableHead  onClick={()=> handleSort('latestPrice')}>
+                <TableHeaderSort
+                  displayTitle="Harga"
+                  title="latestPrice"
+                  sortBy={sortBy}
+                  sortOrder={sortOrder}
+                  setSortOrder={setSortOrder}
+                  setSortBy={setSortBy}
+                />
+                <TableHeaderSort
+                  displayTitle="24 JAM"
+                  title="day"
+                  sortBy={sortBy}
+                  sortOrder={sortOrder}
+                  setSortOrder={setSortOrder}
+                  setSortBy={setSortBy}
+                />
+                <TableHeaderSort
+                  displayTitle="1 MGG"
+                  title="week"
+                  sortBy={sortBy}
+                  sortOrder={sortOrder}
+                  setSortOrder={setSortOrder}
+                  setSortBy={setSortBy}
+                />
+                <TableHeaderSort
+                  displayTitle="1 BLN"
+                  title="month"
+                  sortBy={sortBy}
+                  sortOrder={sortOrder}
+                  setSortOrder={setSortOrder}
+                  setSortBy={setSortBy}
+                />
+                <TableHeaderSort
+                  displayTitle="1 THN"
+                  title="year"
+                  sortBy={sortBy}
+                  sortOrder={sortOrder}
+                  setSortOrder={setSortOrder}
+                  setSortBy={setSortBy}
+                />
+                {/* <TableHead  onClick={()=> handleSort('latestPrice')}>
                   <div className="flex gap-2 mt-4">
                     Harga {sortOrder === "high" && sortBy === 'latestPrice'  ? <FaChevronUp size={18} /> : <FaChevronDown size={18} />}  
                     </div>
@@ -97,60 +157,63 @@ export default function Tables() {
                   <div className="flex gap-2 mt-4">
                     1 THN {(sortOrder === "high" && sortBy === 'year'  ? <FaChevronUp size={18} /> : <FaChevronDown size={18} />)}
                   </div>
-                  </TableHead>
+                  </TableHead> */}
               </TableRow>
             </TableHeader>
             <TableBody>
-              { 
-                sortedData.filter((coin)=>
-                  coin.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                  coin.currencySymbol.toLowerCase().includes(searchTerm.toLowerCase())
-                ).map((curr: {
-                name: string, 
-                currencySymbol: string, 
-                latestPrice: string, 
-                color:string,
-                day: string, 
-                week:string,
-                month:string, 
-                year: string,
-                logo: string
-              }, index: any) => (
-                <TableRow key={index}>
-                  <TableCell className="font-medium" colSpan={2}>
-                    <div className='flex gap-6'>
-                   <CoinLogo url={curr.logo} color={curr.color} />
-                    <div className='flex-col'>
-                    <h1 className='text-lg font-bold'>{curr.name}</h1>
-                    <span className='text-sm text-gray-700'>{curr.currencySymbol}</span>
-                    </div> 
-                    </div>
-                   
-                  </TableCell>
-                  <TableCell>
-                    <h1 className='text-lg font-bold'>
-                    {CurrrencyFormat(curr.latestPrice)}
-                    </h1>
-                  </TableCell>
-                  <TableCellPersentage persentage={curr.day} />
-                  <TableCellPersentage persentage={curr.week} />
-                  <TableCellPersentage persentage={curr.month} />
-                  <TableCellPersentage persentage={curr.year} />
-                  
-                </TableRow>
-              ))
-             }
-
+              {sortedData
+                .filter(
+                  (coin) =>
+                    coin.name
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase()) ||
+                    coin.currencySymbol
+                      .toLowerCase()
+                      .includes(searchTerm.toLowerCase())
+                )
+                .map(
+                  (
+                    curr: {
+                      name: string;
+                      currencySymbol: string;
+                      latestPrice: string;
+                      color: string;
+                      day: string;
+                      week: string;
+                      month: string;
+                      year: string;
+                      logo: string;
+                    },
+                    index: any
+                  ) => (
+                    <TableRow key={index}>
+                      <TableCell className="font-medium" colSpan={2}>
+                        <div className="flex gap-6">
+                          <CoinLogo url={curr.logo} color={curr.color} />
+                          <div className="flex-col">
+                            <h1 className="text-lg font-bold">{curr.name}</h1>
+                            <span className="text-sm text-gray-700">
+                              {curr.currencySymbol}
+                            </span>
+                          </div>
+                        </div>
+                      </TableCell>
+                      <TableCell>
+                        <h1 className="text-lg font-bold">
+                          {CurrrencyFormat(curr.latestPrice)}
+                        </h1>
+                      </TableCell>
+                      <TableCellPersentage persentage={curr.day} />
+                      <TableCellPersentage persentage={curr.week} />
+                      <TableCellPersentage persentage={curr.month} />
+                      <TableCellPersentage persentage={curr.year} />
+                    </TableRow>
+                  )
+                )}
             </TableBody>
           </Table>
-          </div>
-          </div> 
-    )
+        </div>
+      </div>
+    );
   }
 }
-
-
-{/* <TableCell className={`${curr.day.charAt(0) === '-' ? 'text-red-600' : 'text-emerald-600'}`}>{RedGreenIndicator(curr.day)+"%"} </TableCell>
-                  <TableCell className={`${curr.week.charAt(0) === '-' ? 'text-red-600' : 'text-emerald-600'}`}>{curr.week+"%"} </TableCell>
-                  <TableCell className={`${curr.month.charAt(0) === '-' ? 'text-red-600' : 'text-emerald-600'}`}>{curr.month+"%"}</TableCell>
-                  <TableCell className={`${curr.year.charAt(0) === '-' ? 'text-red-600' : 'text-emerald-600'}`}>{curr.year+"%"}</TableCell> */}
