40a71a85e73d9fbe4f163e1c96566c8a41aaf410
sanBastia <irsanoncode@gmail.com>
2024-11-21 12:29:47 +0700

    chore: change to main rather than div

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,27 @@
 import CategoryBadges from "@/components/CategoryBadges";
-import SearchInput from "@/components/SearchInput";
 import Tables from "@/components/Tables";
 
 
 export default async function Home() {
 
   return (
-          <div>
+          <main>
               <header className="w-full">
-                  <nav className="flex justify-between items-center px-10 2xl:px-36 py-6">
+                  <div className="flex justify-between items-center px-10 2xl:px-36 py-6">
                         <div className='flex item-centers'>
                           <h1 className="font-bold font-sans text-2xl">
                           Harga Crypto dalam Rupiah hari ini
                           </h1>
                       </div>
-                      <div className='flex-end items-center'>
-                      <SearchInput />
-                  </div>
-                      </nav>
+                      </div>
                 </header>
-
                   <div className="w-full">
                       <div className="flex justify-around px-10 2xl:px-36">
                         <CategoryBadges />
                        </div>
                   </div>
-                         <Tables />
-              </div>
+                     <Tables />
+              </main>
 
   );
 }
